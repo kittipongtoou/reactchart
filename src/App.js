741793bf63310d5f4afb11699e398c55fa77f4dc
kittipong <45383644+kittipongtoou@users.noreply.github.com>
2020-08-17 22:07:56 +0700

    add

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { DynamicBarChart } from 'react-dynamic-charts';
+import 'react-dynamic-charts/dist/index.css';
+import axios from 'axios';
+
+function transformBarData(data) {
+  let data_covid = [];
+  data.forEach((value) => {
+    var cases = value.timeline.cases;
+    const result = Object.keys(cases).map((key) => cases[key]);
+    if(value.province != null){
+      data_covid.push({'country':value.country + '('+ value.province +')','values':result.slice(-1)[0]})
+    }else{
+      data_covid.push({'country':value.country,'values':result.slice(-1)[0]})
+    }
+ })
+ return {
+  data_covid
+  }
+}
+
+
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getdata(); 
+  }, []);
+
+  const getdata = async () => {
+    const result = await axios(
+      'https://disease.sh/v3/covid-19/historical?lastdays=all',
+    );
+    setData(transformBarData(result.data));
+  };
+  return (
+    <div className="App">
+      <DynamicBarChart
+        data={data}
+      /> 
+    </div>
+  );
+}
+
+export default App;
